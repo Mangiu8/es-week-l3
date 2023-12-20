16d106efa56cb1dc2c5c84c6243e2f774b76c3ff
Antonio Marucci <antoniomarucci13@gmail.com>
2023-12-20 16:35:52 +0100

    input

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,12 +1,23 @@
+import React, { useState } from "react";
 import SingleBook from "./SingleBook";
 
-const BookList = ({ books }) => (
-  <>
-    {books.map((book) => (
-      <SingleBook key={book.asin} book={book} />
-    ))}
-    <input />
-  </>
-);
+const BookList = ({ books }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <>
+      <input type="text" placeholder="Search books..." value={searchTerm} onChange={handleInputChange} />
+      {books
+        .filter((book) => book.title.toLowerCase().includes(searchTerm.toLowerCase()))
+        .map((book) => (
+          <SingleBook key={book.asin} book={book} />
+        ))}
+    </>
+  );
+};
 
 export default BookList;
